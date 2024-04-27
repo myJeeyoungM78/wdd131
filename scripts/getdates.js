81f0dc8ed1267bf58909665b304952a8c7a787c0
kim22008@byui.edu <myJeeyoungM78>
2024-04-27 15:12:38 -0600

    W01-Home Page

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+
+const currentYear = new Date().getFullYear();
+console.log(currentYear);
+document.getElementById("currentyear").innerText = currentYear;
+document.getElementById('lastModified').innerText = document.lastModified;
