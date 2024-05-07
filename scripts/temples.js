8b80c3324e23a18840759b54039404a8d397fc54
kim22008@byui.edu <myJeeyoungM78>
2024-05-07 02:19:11 -0600

    W02-Picture album

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,18 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
-
+// Set footer copyright year
 const currentYear = new Date().getFullYear();
 console.log(currentYear);
 document.getElementById('currentyear').innerText = currentYear;
+
+// Set footer last modified date
 document.getElementById('lastModified').innerText = document.lastModified;
 
-
+// Add event listener to the menu button
 const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
 
 hamButton.addEventListener('click', function() {
     navigation.classList.toggle('open');
     hamButton.classList.toggle('open');
-    document.querySelector('.close-icon').classList.toggle('show');
+    // document.querySelector('.close-icon').classList.toggle('open');
 });
 
 // document.getElementById("menu").addEventListener("click", function() {
