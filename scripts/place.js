182ec81261d38d7bc73ee451a6cca41337db6b6d
kim22008@byui.edu <myJeeyoungM78>
2024-05-17 19:52:50 -0600

    W03-Country Page

--- scripts/place.js
+++ scripts/place.js
@@ -1,12 +1,3 @@
-//set footer content and date
-// const currrentDate = new Date();
-// const yearTag = document.querySelector("#currentyear");
-// const lastModifiedTag = document.querySelector("#lastModified");
-// const lastModified = document.lastModified;
-
-// yearTag.innerHTML = currrentDate.getFullYear();
-// lastModifiedTag.innerHTML = `Last Modification: ${lastModified}`;
-
 // Set footer copyright year
 const currentYear = new Date().getFullYear();
 console.log(currentYear);
@@ -18,29 +9,23 @@ document.getElementById('lastModified').innerText = document.lastModified;
 //set windchill
 const temp = document.querySelector("#temperature").textContent;
 const windSpeed = document.querySelector("#wind").textContent;
-const windChill = document.querySelector("#windChill").textContent;
 
 function calculateWindChill(temp, windSpeed) {
     if (temp <= 50 && windSpeed > 3) {
-        calculatedCill = calculateWindChill(temp, windSpeed);
-        windChill.textContent = `${Math.round(calculateWindChill(temp, windSpeed))}°F`;
-        return 35.74 + (0.6215 * temp) - (35.75 * windSpeed**0.16) + (0.4275 * temp * windSpeed**0.16);
+        const windChillCelsius = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(windSpeed, 0.16)) + 0.4275 * temp * Math.pow(windSpeed, 0.16);
+        return windChillCelsius.toFixed(1);
     } 
     
     else {
         return 'NaN';
     }
-    
 }
 
+document.addEventListener("DOMContentLoaded", function() {
+    const windChillElement = document.getElementById("windChill");
 
+    const windChill = calculateWindChill(temp, windSpeed);
+    windChillElement.textContent = windChill;
+    });
 
-// if (tempature <= 50 && wind > 3) {
-//     calculatedCill = calculateWindChill(tempature, wind);
-//     windChill.textContent = `${Math.round(calculateWindChill(tempature, wind))}°F`;
-// } 
-
-// else {
-//     windChill.textContent = 'NaN';
-// }
 
