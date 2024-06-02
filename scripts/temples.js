0f19abb4aadc65969f11e6bb0f733dad00120f0a
kim22008@byui.edu <myJeeyoungM78>
2024-06-02 04:59:51 -0600

    W06-Website Project

--- scripts/temples.js
+++ scripts/temples.js
@@ -9,23 +9,12 @@ document.getElementById('lastModified').innerText = document.lastModified;
 
 // Add event listener to the menu button
 const hamButton = document.querySelector('#menu');
-const navigation = document.querySelector('.navigation');
+const navigation = document.querySelector('.navigation');``
 
 hamButton.addEventListener('click', function() {
     navigation.classList.toggle('open');
     hamButton.classList.toggle('open');
-    // document.querySelector('.close-icon').classList.toggle('open');
 });
-
-// document.getElementById("menu").addEventListener("click", function() {
-//     var menu = document.getElementById('menu');
-//     this.classList.toggle('active');
-//     if (menu.style.display == 'block') {
-//         menu.style.displaylay = 'none';
-//         }
-//     else {
-//         menu.style.display = 'block';
-//     }
-    
+   
 });
 
